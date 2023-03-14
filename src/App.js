cac3e80ea6a7fe9e6e1758826ffaf8402d8ce627
Selim Yaman <mselimyaman@gmail.com>
2023-03-14 16:26:10 +0300

    router fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import About from "./pages/about/about";
 import Contact from "./pages/contact/contact";
 
 function App() {
   return (
-    <>
-      <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/hakkimda" element={<About/>}/>
-            <Route path="/iletisim" element={<Contact/>}/>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hakkimda" element={<About />} />
+      <Route path="/iletisim" element={<Contact />} />
+    </Routes>
   );
 }
 
